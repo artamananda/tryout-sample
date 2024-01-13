30910ca7ed838e83daf0a711e64238aa5799fa82
Artamananda <artamananda@gmail.com>
2024-01-13 16:41:46 +0700

    adjust migrations

--- backend/db/migrations/20240109034327_create_table_questions.up.sql
+++ backend/db/migrations/20240109034327_create_table_questions.up.sql
@@ -2,9 +2,11 @@ CREATE TABLE IF NOT EXISTS questions (
     question_id UUID PRIMARY KEY,
     tryout_id UUID,
     text TEXT,
+    options JSONB DEFAULT '[]'::jsonb,
     correct_answer VARCHAR(255),
     points INTEGER,
     FOREIGN KEY (tryout_id) REFERENCES tryouts(tryout_id),
     created_at TIMESTAMP,
-    updated_at TIMESTAMP
+    updated_at TIMESTAMP,
+    CHECK (jsonb_typeof(options) = 'array')
 );
--- backend/db/migrations/20240109034334_create_table_options.up.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS options (
-    option_id UUID PRIMARY KEY,
-    question_id UUID,
-    text TEXT,
-    FOREIGN KEY (question_id) REFERENCES questions(question_id)
-);
