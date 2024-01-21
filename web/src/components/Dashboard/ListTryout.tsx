887453cfe5c7fa9a83b2e4bca387bc1b85986c03
Artamananda <artamananda@gmail.com>
2024-01-21 20:50:26 +0700

    add modal token

--- web/src/components/Dashboard/ListTryout.tsx
+++ web/src/components/Dashboard/ListTryout.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import { Table, Tag, Typography } from 'antd';
+import React, { useState } from 'react';
+import { Table, Tag, Typography, Modal, Form, Input } from 'antd';
 import type { TableProps } from 'antd';
 
-const { Text } = Typography;
+const { Text, Link } = Typography;
 
 interface DataType {
   key: string;
@@ -11,75 +11,8 @@ interface DataType {
   endTime: Date;
 }
 
-const columns: TableProps<DataType>['columns'] = [
-  {
-    title: 'Tryout',
-    dataIndex: 'tryout',
-    key: 'tryout'
-  },
-  {
-    title: 'Start Time',
-    dataIndex: 'startTime',
-    key: 'startTime',
-    render: (_, { startTime }) => (
-      <Text>{formatDateToCustomString(startTime)}</Text>
-    )
-  },
-  {
-    title: 'End Time',
-    dataIndex: 'endTime',
-    key: 'endTime',
-    render: (_, { endTime }) => <Text>{formatDateToCustomString(endTime)}</Text>
-  },
-  {
-    title: 'Status',
-    key: 'status',
-    dataIndex: 'status',
-    render: (_, { startTime, endTime }) => (
-      <Tag color={statusColor(checkStatus(startTime, endTime))}>
-        {checkStatus(startTime, endTime)}
-      </Tag>
-    )
-  }
-];
-
-function formatDateToCustomString(date: any) {
-  const options = {
-    year: 'numeric',
-    month: 'long',
-    day: 'numeric',
-    hour: 'numeric',
-    minute: 'numeric',
-    hour12: false
-  };
-
-  const formattedDate = date
-    .toLocaleDateString('id-ID', options)
-    .replace(/,/g, '');
-
-  return formattedDate;
-}
-
-const statusColor = (status: string) => {
-  if (status === 'ON GOING') {
-    return 'green';
-  } else if (status === 'IN COMING') {
-    return 'blue';
-  } else {
-    return 'red';
-  }
-};
-
-const checkStatus = (startTime: Date, endTime: Date) => {
-  if (startTime < new Date() && endTime > new Date()) {
-    return 'ON GOING';
-  } else if (startTime > new Date()) {
-    return 'IN COMING';
-  } else if (endTime < new Date()) {
-    return 'FINISHED';
-  } else {
-    return 'UNKNOWN';
-  }
+type FieldType = {
+  token?: string;
 };
 
 const data: DataType[] = [
@@ -104,7 +37,130 @@ const data: DataType[] = [
 ];
 
 const ListTryout = () => {
-  return <Table columns={columns} dataSource={data} />;
+  const columns: TableProps<DataType>['columns'] = [
+    {
+      title: 'Tryout',
+      dataIndex: 'tryout',
+      key: 'tryout',
+      render: (_, record) => (
+        <Link underline onClick={() => showModal(record.tryout)}>
+          {record.tryout}
+        </Link>
+      )
+    },
+    {
+      title: 'Start Time',
+      dataIndex: 'startTime',
+      key: 'startTime',
+      render: (_, { startTime }) => (
+        <Text>{formatDateToCustomString(startTime)}</Text>
+      )
+    },
+    {
+      title: 'End Time',
+      dataIndex: 'endTime',
+      key: 'endTime',
+      render: (_, { endTime }) => (
+        <Text>{formatDateToCustomString(endTime)}</Text>
+      )
+    },
+    {
+      title: 'Status',
+      key: 'status',
+      dataIndex: 'status',
+      render: (_, { startTime, endTime }) => (
+        <Tag color={statusColor(checkStatus(startTime, endTime))}>
+          {checkStatus(startTime, endTime)}
+        </Tag>
+      )
+    }
+  ];
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [modalTitle, setModalTitle] = useState('Input Token');
+  const [form] = Form.useForm();
+
+  const showModal = (modalTitle: string) => {
+    setModalTitle(modalTitle);
+    setIsModalOpen(true);
+  };
+
+  function formatDateToCustomString(date: any) {
+    const options = {
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric',
+      hour: 'numeric',
+      minute: 'numeric',
+      hour12: false
+    };
+
+    const formattedDate = date
+      .toLocaleDateString('id-ID', options)
+      .replace(/,/g, '');
+
+    return formattedDate;
+  }
+
+  const statusColor = (status: string) => {
+    if (status === 'ON GOING') {
+      return 'green';
+    } else if (status === 'IN COMING') {
+      return 'blue';
+    } else {
+      return 'red';
+    }
+  };
+
+  const checkStatus = (startTime: Date, endTime: Date) => {
+    if (startTime < new Date() && endTime > new Date()) {
+      return 'ON GOING';
+    } else if (startTime > new Date()) {
+      return 'IN COMING';
+    } else if (endTime < new Date()) {
+      return 'FINISHED';
+    } else {
+      return 'UNKNOWN';
+    }
+  };
+
+  const onFinish = (values: any) => {
+    console.log('Success:', values);
+  };
+
+  const onFinishFailed = (errorInfo: any) => {
+    console.log('Failed:', errorInfo);
+  };
+
+  return (
+    <div>
+      <Table columns={columns} dataSource={data} />
+
+      <Modal
+        title={modalTitle}
+        open={isModalOpen}
+        onCancel={() => setIsModalOpen(false)}
+        onOk={form.submit}
+      >
+        <Form
+          form={form}
+          name="basic"
+          onFinish={onFinish}
+          onFinishFailed={onFinishFailed}
+          autoComplete="off"
+          layout="vertical"
+        >
+          <Form.Item<FieldType>
+            label="Token"
+            name="token"
+            rules={[{ required: true, message: 'Please input your token!' }]}
+          >
+            <Input />
+          </Form.Item>
+        </Form>
+      </Modal>
+    </div>
+  );
 };
 
 export default ListTryout;
