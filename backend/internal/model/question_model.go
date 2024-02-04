6905f9f9cd6aed159d5c88a6873747536369186a
Artamananda <artamananda@gmail.com>
2024-02-04 18:02:18 +0700

    adjust validation question

--- backend/internal/model/question_model.go
+++ backend/internal/model/question_model.go
@@ -7,26 +7,26 @@ import (
 )
 
 type CreateQuestionRequest struct {
-	Text          string          `json:"text" validate:"required"`
-	Options       []string `json:"options" validate:"required,min=1,dive"`
-	CorrectAnswer string          `json:"correct_answer" validate:"required"`
-	Points        int             `json:"points" validate:"required"`
+	Text          string   `json:"text" validate:"required"`
+	Options       []string `json:"options"`
+	CorrectAnswer string   `json:"correct_answer" validate:"required"`
+	Points        int      `json:"points"`
 }
 
 type UpdateQuestionRequest struct {
-	Text          string          `json:"text"`
+	Text          string   `json:"text"`
 	Options       []string `json:"options" validate:"min=1,dive"`
-	CorrectAnswer string          `json:"correct_answer"`
-	Points        int             `json:"points"`
+	CorrectAnswer string   `json:"correct_answer"`
+	Points        int      `json:"points"`
 }
 
 type QuestionResponse struct {
-	QuestionID    uuid.UUID       `json:"question_id"`
-	TryoutID      uuid.UUID       `json:"tryout_id"`
-	Text          string          `json:"text"`
-	Options       []string `json:"options"`
-	CorrectAnswer string          `json:"correct_answer"`
-	Points        int             `json:"points"`
-	CreatedAt     time.Time       `json:"created_at"`
-	UpdatedAt     time.Time       `json:"updated_at"`
+	QuestionID    uuid.UUID `json:"question_id"`
+	TryoutID      uuid.UUID `json:"tryout_id"`
+	Text          string    `json:"text"`
+	Options       []string  `json:"options"`
+	CorrectAnswer string    `json:"correct_answer"`
+	Points        int       `json:"points"`
+	CreatedAt     time.Time `json:"created_at"`
+	UpdatedAt     time.Time `json:"updated_at"`
 }
