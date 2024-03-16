ce44495d10fa38554618ea4e37708fef5621eeb2
Artamananda <artamananda@gmail.com>
2024-03-17 06:10:12 +0700

    remove panic

--- backend/internal/repository/user_answer_repository.go
+++ backend/internal/repository/user_answer_repository.go
@@ -3,9 +3,9 @@ package repository
 import (
 	"context"
 	"errors"
+	"fmt"
 
 	"github.com/artamananda/tryout-sample/internal/entity"
-	"github.com/artamananda/tryout-sample/internal/exception"
 	"github.com/google/uuid"
 	"gorm.io/gorm"
 )
@@ -21,20 +21,20 @@ func NewUserAnswerRepository(DB *gorm.DB) UserAnswerRepository {
 func (repository *UserAnswerRepository) Create(ctx context.Context, user_answer entity.UserAnswer) entity.UserAnswer {
 	user_answer.UserAnswerID = uuid.New()
 	err := repository.DB.WithContext(ctx).Create(&user_answer).Error
-	exception.PanicLogging(err)
+	fmt.Println(err)
 	return user_answer
 }
 
 func (repository *UserAnswerRepository) Update(ctx context.Context, user_answer entity.UserAnswer) entity.UserAnswer {
 	err := repository.DB.WithContext(ctx).Where("user_answer_id = ?", user_answer.UserAnswerID).Updates(&user_answer).Error
-	exception.PanicLogging(err)
+	fmt.Println(err)
 
 	return user_answer
 }
 
 func (repository *UserAnswerRepository) Delete(ctx context.Context, user_answer entity.UserAnswer) {
 	err := repository.DB.WithContext(ctx).Where("user_answer_id = ?", user_answer.UserAnswerID).Delete(&user_answer).Error
-	exception.PanicLogging(err)
+	fmt.Println(err)
 }
 
 func (repository *UserAnswerRepository) FindById(ctx context.Context, user_answer_id string) (entity.UserAnswer, error) {
