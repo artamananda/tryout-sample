fda67a25409cf763192892939262a5b5e832fcb5
Artamananda <artamananda@gmail.com>
2024-05-01 21:54:06 +0700

    update version

--- backend/main.go
+++ backend/main.go
@@ -4,6 +4,7 @@ import (
 	"github.com/artamananda/tryout-sample/internal/config"
 	"github.com/artamananda/tryout-sample/internal/controller"
 	"github.com/artamananda/tryout-sample/internal/exception"
+	"github.com/artamananda/tryout-sample/internal/model"
 	"github.com/artamananda/tryout-sample/internal/repository"
 	"github.com/artamananda/tryout-sample/internal/service"
 	"github.com/gofiber/fiber/v2"
@@ -16,12 +17,11 @@ func main() {
 	app := fiber.New()
 
 	app.Use(cors.New(cors.Config{
-        AllowHeaders:     "*",
-        AllowOrigins:     "*",
-        AllowCredentials: true,
-        AllowMethods:     "GET,POST,HEAD,PUT,DELETE,PATCH,OPTIONS",
-    }))
-
+		AllowHeaders:     "*",
+		AllowOrigins:     "*",
+		AllowCredentials: true,
+		AllowMethods:     "GET,POST,HEAD,PUT,DELETE,PATCH,OPTIONS",
+	}))
 
 	userRepository := repository.NewUserRepository(db)
 	tryoutRepository := repository.NewTryoutRepository(db)
@@ -38,14 +38,17 @@ func main() {
 	questionController := controller.NewQuestionController(&questionService, initConfig)
 	userAnswerController := controller.NewUserAnswerController(&userAnswerService, initConfig)
 
-	
 	userController.Route(app)
 	tryoutController.Route(app)
 	questionController.Route(app)
 	userAnswerController.Route(app)
 
 	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, Fiber!")
+		return c.Status(fiber.StatusCreated).JSON(model.GeneralResponse{
+			Code:    200,
+			Message: "Success",
+			Data:    "0.0.11",
+		})
 	})
 
 	app.Get("/hello/:name", func(c *fiber.Ctx) error {
