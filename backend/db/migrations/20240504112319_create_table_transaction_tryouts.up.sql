36540a73961eb630dbab36a51dcbac8f26577ee9
Artamananda <artamananda@gmail.com>
2024-05-04 19:09:10 +0700

    add migrations

---
+++ backend/db/migrations/20240504112319_create_table_transaction_tryouts.up.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS transaction_tryouts (
+    transaction_tryout_id UUID PRIMARY KEY,
+    tryout_id UUID,
+    user_id UUID,
+    status VARCHAR(100),
+    start_time TIMESTAMP,
+    end_time TIMESTAMP,
+    created_at TIMESTAMP,
+    updated_at TIMESTAMP
+);
