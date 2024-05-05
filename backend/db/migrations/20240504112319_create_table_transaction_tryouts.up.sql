a28d27a5b005c1dff77088c4a4937858fc756b56
Artamananda <artamananda@gmail.com>
2024-05-05 18:36:11 +0700

    adjust migrations

--- backend/db/migrations/20240504112319_create_table_transaction_tryouts.up.sql
+++ backend/db/migrations/20240504112319_create_table_transaction_tryouts.up.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS transaction_tryouts (
     transaction_tryout_id UUID PRIMARY KEY,
     tryout_id UUID,
     user_id UUID,
-    status VARCHAR(100),
+    status VARCHAR(50),
     start_time TIMESTAMP,
     end_time TIMESTAMP,
     created_at TIMESTAMP,
