de196fc360f7fa8d8e98da496388a9aaf0c6ba59
Artamananda <artamananda@gmail.com>
2024-07-07 15:21:43 +0700

    fix: error install.sh

--- backend/main.go
+++ backend/main.go
@@ -15,10 +15,10 @@ import (
 	_ "github.com/artamananda/tryout-sample/docs"
 )
 
-const APP_VERSION = "0.0.34"
+const APP_VERSION = "0.0.35"
 
 // @title Tryout Sample
-// @version 0.0.34
+// @version 0.0.35
 // @description API Documentation for Telisik Tryout
 // @termsOfService http://swagger.io/terms/
 // @contact.name Artamananda
