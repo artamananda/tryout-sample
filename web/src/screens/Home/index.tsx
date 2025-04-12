6e2d7bd462f0d6bd02ff2867c6bacfd36ac3f703
Artamananda <artamananda@gmail.com>
2025-04-12 20:52:41 +0700

    WIP: landing page

--- web/src/screens/Home/index.tsx
+++ web/src/screens/Home/index.tsx
@@ -1,110 +1,41 @@
-import { Button, Dropdown, Image, Layout, message, Typography } from 'antd';
-import logo from '../../assets/logo-yellow.png';
-import type { MenuProps } from 'antd';
-import { useAuthUser, useSignOut } from 'react-auth-kit';
-import { useNavigate } from 'react-router-dom';
-import { DownOutlined, UserOutlined } from '@ant-design/icons';
-import { EColor } from '../../constants/color';
-import backgroundImage1 from '../../assets/background1.jpg';
-import backgroundImage2 from '../../assets/background2.jpg';
-import { useEffect, useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { Navigation } from '../../components/Home/Navigation';
+import { Header } from '../../components/Home/Header';
+import { Features } from '../../components/Home/Features';
+import { About } from '../../components/Home/About';
+import { Services } from '../../components/Home/Services';
+import { Gallery } from '../../components/Home/Galery';
+import { Testimonials } from '../../components/Home/Testimonial';
+import { Team } from '../../components/Home/Team';
+import { Contact } from '../../components/Home/Contact';
+import JsonData from '../../constants/data.json';
+import SmoothScroll from 'smooth-scroll';
+import './Home.css';
 
-const { Header, Content } = Layout;
-const { Text, Title } = Typography;
+export const scroll = new SmoothScroll('a[href*="#"]', {
+  speed: 1000,
+  speedAsDuration: true
+});
 
-const HomeScreen = () => {
-  const signOut = useSignOut();
-  const userAuth = useAuthUser();
-  const authName = userAuth() ? userAuth()?.name : null;
-  const navigate = useNavigate();
-
-  const [backgroundIndex, setBackgroundIndex] = useState(0);
-  const backgrounds = [backgroundImage1, backgroundImage2];
-
+const App = () => {
+  const [landingPageData, setLandingPageData] = useState<any>({});
   useEffect(() => {
-    const intervalId = setInterval(() => {
-      setTimeout(() => {
-        setBackgroundIndex((prevIndex) =>
-          prevIndex === backgrounds.length - 1 ? 0 : prevIndex + 1
-        );
-      }, 1000);
-    }, 5000);
+    setLandingPageData(JsonData);
+  }, []);
 
-    return () => clearInterval(intervalId);
-  }, [backgrounds.length]);
-
-  const items: MenuProps['items'] = [
-    {
-      key: '1',
-      label: (
-        <text
-          onClick={() => {
-            signOut();
-            navigate('/');
-            message.success("You've been signed out");
-          }}
-        >
-          Logout
-        </text>
-      )
-    }
-  ];
   return (
-    <Layout
-      style={{
-        minHeight: '100vh',
-        backgroundColor: 'white',
-        backgroundImage: `url(${backgrounds[backgroundIndex]})`,
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
-        transition: 'background-image 1s ease-in-out',
-        transitionProperty: 'background-image'
-      }}
-    >
-      <Header
-        style={{
-          backgroundColor: EColor.PRIMARY,
-          display: 'flex',
-          flexDirection: 'row',
-          alignItems: 'center',
-          justifyContent: 'space-between'
-        }}
-      >
-        <Image src={logo} width={120} preview={false} />
-        {authName ? (
-          <Dropdown menu={{ items }} placement="bottomLeft">
-            <div style={{ color: 'white' }}>
-              <UserOutlined style={{ marginRight: 5 }} />
-              {authName} <DownOutlined style={{ marginLeft: 10 }} />
-            </div>
-          </Dropdown>
-        ) : (
-          <Button type="primary" onClick={() => navigate('/login')}>
-            Login
-          </Button>
-        )}
-      </Header>
-      <Content
-        style={{
-          display: 'flex',
-          flexDirection: 'row',
-          justifyContent: 'center',
-          alignItems: 'center'
-        }}
-      >
-        <div style={{ textAlign: 'center' }}>
-          <Title style={{ fontWeight: 'bold', color: 'white' }}>
-            Teras Belajar Asik
-          </Title>
-          <Text style={{ color: 'white' }}>
-            {
-              'Belajar UTBK ASIK (Agamis, Saintis, Sosial, Skolastik). Program gratis bagi anak Sumsel menuju PTN.'
-            }
-          </Text>
-        </div>
-      </Content>
-    </Layout>
+    <div>
+      <Navigation />
+      <Header data={landingPageData.Header} />
+      {/* <Features data={landingPageData.Features} /> */}
+      <About data={landingPageData.About} />
+      <Services data={landingPageData.Services} />
+      <Gallery data={landingPageData.Gallery} />
+      <Testimonials data={landingPageData.Testimonials} />
+      <Team data={landingPageData.Team} />
+      <Contact data={landingPageData.Contact} />
+    </div>
   );
 };
 
-export default HomeScreen;
+export default App;
