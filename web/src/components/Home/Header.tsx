403c6a1932778dd237fa31adf8f56674e213f3c7
Artamananda <artamananda@gmail.com>
2025-04-13 11:51:43 +0700

    adjust hero

--- web/src/components/Home/Header.tsx
+++ web/src/components/Home/Header.tsx
@@ -1,23 +1,33 @@
 import React from 'react';
 import GradientText from './GradientText';
+import './Header.css';
 
 export const Header = (props: any) => {
   return (
-    <header id="header">
-      <div className="intro">
-        <div className="overlay">
-          <div className="container">
-            <div className="row">
-              <div className="col-md-8 col-md-offset-2 intro-text">
-                <GradientText>
-                  {props.data ? props.data.title : 'Loading'}
-                </GradientText>
-                <p>{props.data ? props.data.paragraph : 'Loading'}</p>
-                <a href="#about" className="btn btn-custom btn-lg page-scroll">
-                  SELENGKAPNYA
-                </a>{' '}
-              </div>
+    <header id="header" className="header">
+      <div className="header-container">
+        <div className="header-content">
+          <div className="header-text">
+            <GradientText>
+              {props.data ? props.data.title : 'Loading'}
+            </GradientText>
+            <p>{props.data ? props.data.paragraph : 'Loading'}</p>
+            <div className="header-buttons">
+              <a href="#about" className="btn btn-custom page-scroll">
+                SELENGKAPNYA
+              </a>
+              <a href="/tryout" className="btn btn-custom page-scroll">
+                TRYOUT ONLINE
+              </a>
             </div>
+          </div>
+
+          <div className="header-image">
+            <img
+              src={process.env.PUBLIC_URL + '/img/model.png'}
+              alt="Vector Telisik"
+              className="responsive-img"
+            />
           </div>
         </div>
       </div>
