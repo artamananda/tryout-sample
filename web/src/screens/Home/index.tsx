df4aca1fb7185a780654cd41a982ad13ef75d59f
artamananda <artamananda@gmail.com>
2026-03-24 13:18:09 +0700

    fix: comment out Features component in HomeScreen

--- web/src/screens/Home/index.tsx
+++ web/src/screens/Home/index.tsx
@@ -29,7 +29,7 @@ const HomeScreen = () => {
       <ScrollToHash />
       <Navigation />
       <Header data={landingPageData.Header} />
-      <Features targetDate="2025-12-20 09:00:00" />
+      {/* <Features targetDate="2025-12-20 09:00:00" /> */}
       <About data={landingPageData.About} />
       <Services data={landingPageData.Services} />
       <Gallery data={landingPageData.Gallery} />
