bf15ab45c132ebf9e5402552906a6a73ebedbadb
artamananda <artamananda@gmail.com>
2026-05-31 22:23:52 +0700

    update version

--- backend/main.go
+++ backend/main.go
@@ -17,10 +17,10 @@ import (
 	_ "github.com/artamananda/tryout-sample/docs"
 )
 
-const APP_VERSION = "0.5.3"
+const APP_VERSION = "0.6.0"
 
 // @title Tryout Sample
-// @version 0.5.3
+// @version 0.6.0
 // @description API Documentation for Telisik Tryout
 // @termsOfService http://swagger.io/terms/
 // @contact.name Artamananda
